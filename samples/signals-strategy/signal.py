380ec5cc7c6661bfc7f79f4c84f7640895d4ab70
Peter Chong <peter@kpex.asia>
2019-05-19 15:00:52 +0800

    first commit

---
+++ samples/signals-strategy/signal.py
@@ -0,0 +1,10 @@
+import collections
+import backtrader as bt
+
+MAINSIGNALS = collections.OrderedDict(
+    (('longshort', bt.SIGNAL_LONGSHORT),
+     ('longonly', bt.SIGNAL_LONG),
+     ('shortonly', bt.SIGNAL_SHORT),)
+)
+
+print(list(MAINSIGNALS.keys())[0])
